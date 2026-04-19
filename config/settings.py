019d8e50689bcc8e653e74724a7f0475107658f3
dzakyadr <muhadrian061@gmail.com>
2026-04-19 20:28:46 +0700

    final setup for render

--- config/settings.py
+++ config/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST'),      
         'PORT': os.getenv('DB_PORT'),  
         'OPTIONS': {
-            'sslmode': 'require',     
+            'sslmode': 'require',
         }
     }
 }
